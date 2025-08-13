6566899630563d2fb11961f6695f71190f69d2b5
der-faebu <fabian@gygax.tech>
2025-08-13 23:52:20 +0200

    variable consolidating

--- templates/compile.sh
+++ templates/compile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-root="{{ home }}/{{ azerothcore_folder }}"
-cd "$root/{{ azerothcore_source_dest_dir }}"
+# root="{{ home }}/{{ azerothcore_folder }}" -> checkout
+# cd "$root/{{ azerothcore_source_dest_dir }}" -> src
+
+root="{{ azerothcore_checkout_path }}"
+cd "$root/{{ azerothcore_source_dir }}"
 mkdir -p build/
 cd build
 
-cmake ../ -DCMAKE_INSTALL_PREFIX="$root/{{ azerothcore_server_release }}/" \
+cmake ../ -DCMAKE_INSTALL_PREFIX="$root/{{ azerothcore_build_dir }}/" \
         -DCMAKE_C_COMPILER=/usr/bin/clang \
         -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
         -DWITH_WARNINGS=1 \
